2a90d4c700b3dcf4f8e26048897ecc6aa3ad7a22
paradoxical-dev <jaredonnell21@gmail.com>
2026-03-09 19:26:19 -0500

    feat(theme): updated new theme implementation

---
+++ current-theme.el
@@ -0,0 +1,19 @@
+;;; current-theme.el --- Sets the currently selected theme -*- lexical-binding: t; -*-
+
+;;
+;; ROSE PINE
+;;
+
+(setq doom-theme 'doom-everforest)
+
+;;
+;; NANO
+;;
+
+;; (require 'nano-modeline)
+;; (require 'nano-base-colors)
+;; (require 'nano-faces)
+;; (require 'nano-theme)
+;; (nano-mode)
+;; (load-theme 'nano-light t)
+;; (require 'nano-modeline)
