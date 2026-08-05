5d0b9b06674d3d2d1eae7379a7bcc3b65aa086c3
paradoxical-dev <jaredonnell21@gmail.com>
2026-08-04 22:35:46 -0500

    feat(editor): mu4e overhaul

--- current-theme.el
+++ current-theme.el
@@ -4,7 +4,14 @@
 ;; NORD
 ;;
 
-(setq doom-theme 'doom-nord)
+;; (setq doom-theme 'doom-nord)
+;; (setq doom-theme 'doom-nord-light)
+
+;;
+;; EARL GREY
+;;
+
+(setq doom-theme 'doom-earl-grey)
 
 ;;
 ;; ROSE PINE
@@ -16,10 +23,19 @@
 ;; NANO
 ;;
 
-;; (require 'nano-modeline)
 ;; (require 'nano-base-colors)
 ;; (require 'nano-faces)
 ;; (require 'nano-theme)
 ;; (nano-mode)
 ;; (load-theme 'nano-light t)
 ;; (require 'nano-modeline)
+;; (custom-set-faces
+;;  '(org-quote                    ((t (:background "#f3f5f6" :extend t))))
+;;  '(org-block                    ((t (:background "#f3f5f6"))))
+;;  '(org-block-begin-line         ((t (:background "#f3f5f6"))))
+;;  '(org-block-end-line           ((t (:background "#f3f5f6")))))
+;; (after! nano-mu4e
+;;   (custom-set-faces
+;;    '(nano-mu4e-tag-active   ((t (:foreground "pink" :background "#ffffff" :weight bold))))
+;;    '(nano-mu4e-tag-inactive ((t (:foreground "#673AB7" :background "#ffffff" :weight bold))))
+;;    '(nano-mu4e-todo         ((t (:foreground "#FF6F00" :background "#ffffff" :weight bold))))))
